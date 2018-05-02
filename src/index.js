1207adfcf43867b0ed724754a881e85f9e97b330
Aleksandra Krzywicka <aleksandrakrzywicka@gmail.com>
2018-05-02 21:00:15 +0200

    Add project

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import store from './store';
+import routes from './routes';
+import DevTools from './dev-tools.js';
+
+
+render(
+	<Provider store={store}>
+		<div>
+			<Router history={hashHistory} routes={routes}/>
+			<DevTools />
+		</div>
+	</ Provider>,
+	document.getElementById('root')
+);
+
